1f8219b2c7fde1799cee0e92735bf143ddcb9570
Guilherme China <echina725@gmail.com>
2024-06-11 18:35:33 -0300

    adds some movement

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 _G.love = require "love"
+_G.keyboard = love.keyboard
 
 -- called only once at the beggining of the game
 function love.load()
@@ -7,39 +8,59 @@ function love.load()
         x = 500,
         y = 200,
         move_forward = function(self)
-            self.x = self.x + 1
+            self.x = self.x + 3
         end,
-        is_eating = false
+        move_backward = function(self)
+            self.x = self.x - 3
+        end,
+        move_up = function(self)
+            self.y = self.y - 3
+        end,
+        move_down = function(self)
+            self.y = self.y + 3
+        end
     }
-    Food_x = 680
-
+    Food = {
+        x = 600,
+        y = 300,
+        eaten = false
+    }
     love.graphics.setBackgroundColor(74 / 255, 14 / 255, 159 / 255)
 end
 
 -- callback called that update the state of the game every frame
 --- @param dt integer deltatime
 function love.update(dt)
-    pacman:move_forward();
-    if pacman.x >= (Food_x - 20) then
-        pacman.is_eating = true
+    if keyboard.isDown("w") then
+        pacman:move_up();
+    end
+    if keyboard.isDown("d") then
+        pacman:move_forward();
+    end
+    if keyboard.isDown("a") then
+        pacman:move_backward();
+    end
+    if keyboard.isDown("s") then
+        pacman:move_down();
+    end
+    if pacman.x >= (Food.x - 20) then
+        Food.eaten = true
     end
 end
 
 -- Callback function used to draw on the screen every frame.
 function love.draw()
-    --love.graphics.setColor(Number, Number / 2, 0, 1 / Number)
     love.graphics.setColor(75 / 255, 148 / 255, 10 / 255)
     love.graphics.rectangle("fill", 200, 200, 150, 100)
     love.graphics.setColor(255 / 255, 196 / 255, 0)
-    love.graphics.arc("fill", pacman.x, 200, 60, 1, 5)
+    love.graphics.arc("fill", pacman.x, pacman.y, 60, 2, 6)
     love.graphics.setColor(0, 0, 0)
     love.graphics.line(350, 200, 500, 200)
     love.graphics.circle("fill", 500, 500, 100, 100)
     love.graphics.setColor(1, 0, 0, 1)
     love.graphics.line(500, 201, 500, 500)
-
-    if not pacman.is_eating then
+    if not Food.eaten then
         love.graphics.setColor(1, 0, 0)
-        love.graphics.circle("fill", Food_x, pacman.y, 20)
+        love.graphics.circle("fill", Food.x, Food.y, 20)
     end
 end
